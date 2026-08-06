af5ec376262445e2bd3c1a6d9c671709491d8737
GregLem <grigorijlemisak@gmail.com>
2026-08-06 18:18:13 +0300

    Завершён этап аутентификации (myaut): - Добавлен выход через LogoutView (CBV) - Реализованы куки: set_cookie_view, get_cookie_view - Реализованы сессии: set_session_view, get_session_view - Настроены безопасные редиректы через reverse_lazy - Вход через LoginView с кастомным шаблоном - Подключены маршруты /myaut/login/, /myaut/logout/ - Добавлены настройки LOGIN_URL, LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL

--- mysite/myaut/urls.py
+++ mysite/myaut/urls.py
@@ -1,11 +1,26 @@
 from django.urls import path
 from .views import login_view
 from django.contrib.auth.views import LoginView
-
+from . views import (set_cookie_view,
+                    get_cookie_view,
+                    set_session_view, 
+                    get_session_view,
+                    logout_view,
+                    MyLogoutView)
 
 app_name = "myaut"
 
 urlpatterns = [
     #path("login/", login_view, name="login"),
-    path("login/", LoginView.as_view(template_name="myaut/login.html", redirect_authenticated_user=True), name="login"),
+    path("login/", LoginView.as_view(template_name="myaut/login.html",
+                                    redirect_authenticated_user=True),
+                                    name="login"),
+    path("cookie/set/", set_cookie_view, name="set_cookie"),
+    path("cookie/get/", get_cookie_view, name="get_cookie"),
+
+    path("session/set/", set_session_view, name="set_session"),
+    path("session/get/", get_session_view, name="get_session"),
+
+    #path("logout/", logout_view, name="logout"),
+    path("logout/", MyLogoutView.as_view(), name="logout"),
 ]
--- mysite/myaut/views.py
+++ mysite/myaut/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.views import LogoutView
 
 def login_view(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
         if request.user.is_authenticated:
             return redirect("/admin/")
-        return render(request, "myauth/login.html")
+        return render(request, "myaut/login.html")
     
     username = request.POST.get("username")
     password = request.POST.get("password")
@@ -15,3 +17,27 @@ def login_view(request: HttpRequest) -> HttpResponse:
         login(request, user)
         return redirect("/admin/")
     return render(request, "myaut/login.html", context={"error": "Invalid username or password"})
+
+def logout_view(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect(reverse("myaut:login"))
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy("myaut:login")
+
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse("Cookie has been set.")
+    response.set_cookie("fizz", "buzz", max_age=3600)  # Cookie expires in 1 hour   
+    return response
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get("fizz", "Default Value")
+    return HttpResponse(f"Cookie value: {value}")
+
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.session["foobar"] = "spameggs"
+    return HttpResponse("Session value has been set.")
+
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get("foobar", "Default Value")
+    return HttpResponse(f"Session value: {value}")
