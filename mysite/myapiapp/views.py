6a4b1636fb6ef89d9a3fa91cbffda752f081e331
GregLem <grigorijlemisak@gmail.com>
2026-09-24 17:52:08 +0300

    Добавлен API списка групп и GroupSerializer

--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -1,10 +1,20 @@
+from django.contrib.auth.models import Group
 from django.shortcuts import render
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
+from .serializers import  GroupSerializer
 
 
 @api_view(['GET'])
 def hello_world_view(request: Request) -> Response:
     
     return Response({"message": "Hello, world!"})
+
+class GroupsListView(APIView):
+    def get(self, request: Request) -> Response:
+        groups = Group.objects.all()
+        serializer = GroupSerializer(groups, many=True)
+        return Response({"groups": serializer.data})
